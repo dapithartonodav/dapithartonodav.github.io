c1f4c85dbe3652d45001fb19d0af783619471453
dapithartonodav <dapithartono2@gmail.com>
2021-05-10 16:46:01 +0700

    initial commit

---
+++ Javascript/manipulasi_konten.js
@@ -0,0 +1,6 @@
+var elems = document.getElementsByClassName('warna');
+for(var i = 0; i < elems.length; i++) {
+    elems[i].style.background= '#696463';
+    elems[i].style.width='100%';
+    elems[i].style.padding='15px';
+}
